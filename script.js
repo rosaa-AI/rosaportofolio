074435ea3f18d2d0d712c8267ed385ba31ff4152
Rosa Silaban <silabanrosa4@gmail.com>
2026-03-30 22:39:08 +0700

    first upload portfolio

---
+++ script.js
@@ -0,0 +1,17 @@
+// Animasi muncul saat scroll
+const sections = document.querySelectorAll("section");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.opacity = 1;
+      entry.target.style.transform = "translateY(0)";
+    }
+  });
+});
+
+sections.forEach(sec => {
+  sec.style.opacity = 0;
+  sec.style.transform = "translateY(50px)";
+  observer.observe(sec);
+});
